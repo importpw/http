50c4a87122575f2609f09c8bd8150e52d9ea32a4
Nathan Rajlich <nathan@tootallnate.net>
2018-09-03 14:41:34 -0700

    Remove legacy cruft
    Static file serving will be implemented in a different module.
    
    The core `http` is only concerned about implementing the HTTP
    protocol itself. Perhaps `zeit/serve` can have a shell implementation.

--- bashttpd.sh
+++ bashttpd.sh
@@ -8,108 +8,7 @@
 # Reworked by:     Josh Cartwright, 2012
 # Reworked by:     Nathan Rajlich, 2018
 
-# If running as `root`, then downgrade to `nobody` first
-if [ "$EUID" -eq 0 ]; then
-  exec sudo -u nobody "$0" "$@"
-fi
-
 conf="${BASHTTPD_CONFIG-bashttpd.conf}"
-
-[ -r "${conf}" ] || {
-   cat > "${conf}" <<'EOF'
-#
-# bashttpd.conf - configuration for bashttpd
-#
-# The behavior of bashttpd is dictated by the evaluation
-# of rules specified in this configuration file.  Each rule
-# is evaluated until one is matched.  If no rule is matched,
-# bashttpd will serve a 500 Internal Server Error.
-#
-# The format of the rules are:
-#    on_uri_match REGEX command [args]
-#    unconditionally command [args]
-#
-# on_uri_match:
-#   On an incoming request, the URI is checked against the specified
-#   (bash-supported extended) regular expression, and if encounters a match the
-#   specified command is executed with the specified arguments.
-#
-#   For additional flexibility, on_uri_match will also pass the results of the
-#   regular expression match, ${BASH_REMATCH[@]} as additional arguments to the
-#   command.
-#
-# unconditionally:
-#   Always serve via the specified command.  Useful for catchall rules.
-#
-# The following commands are available for use:
-#
-#   serve_file FILE
-#     Statically serves a single file.
-#
-#   serve_dir_with_tree DIRECTORY
-#     Statically serves the specified directory using 'tree'.  It must be
-#     installed and in the PATH.
-#
-#   serve_dir_with_ls DIRECTORY
-#     Statically serves the specified directory using 'ls -al'.
-#
-#   serve_dir  DIRECTORY
-#     Statically serves a single directory listing.  Will use 'tree' if it is
-#     installed and in the PATH, otherwise, 'ls -al'
-#
-#   serve_dir_or_file_from DIRECTORY
-#     Serves either a directory listing (using serve_dir) or a file (using
-#     serve_file).  Constructs local path by appending the specified root
-#     directory, and the URI portion of the client request.
-#
-#   serve_static_string STRING
-#     Serves the specified static string with Content-Type text/plain.
-#
-# Examples of rules:
-#
-# on_uri_match '^/issue$' serve_file "/etc/issue"
-#
-#   When a client's requested URI matches the string '/issue', serve them the
-#   contents of /etc/issue
-#
-# on_uri_match 'root' serve_dir /
-#
-#   When a client's requested URI has the word 'root' in it, serve up
-#   a directory listing of /
-#
-# DOCROOT=/var/www/html
-# on_uri_match '/(.*)' serve_dir_or_file_from "$DOCROOT"
-#   When any URI request is made, attempt to serve a directory listing
-#   or file content based on the request URI, by mapping URI's to local
-#   paths relative to the specified "$DOCROOT"
-#
-
-unconditionally serve_static_string 'Hello, world!  You can configure bashttpd by modifying bashttpd.conf.'
-
-# More about commands:
-#
-# It is possible to somewhat easily write your own commands.  An example
-# may help.  The following example will serve "Hello, $x!" whenever
-# a client sends a request with the URI /say_hello_to/$x:
-#
-# serve_hello() {
-#    set_response_header "Content-Type" "text/plain"
-#    echo "Hello, $2!"
-# }
-# on_uri_match '^/say_hello_to/(.*)$' serve_hello
-#
-# Like mentioned before, the contents of ${BASH_REMATCH[@]} are passed
-# to your command, so its possible to use regular expression groups
-# to pull out info.
-#
-# With this example, when the requested URI is /say_hello_to/Josh, serve_hello
-# is invoked with the arguments '/say_hello_to/Josh' 'Josh',
-# (${BASH_REMATCH[0]} is always the full match)
-EOF
-   echo "Created bashttpd.conf using defaults."
-   echo "Please review it/configure before running bashttpd again."
-   exit 1
-}
 
 recv() { echo "<" "$@" >&2; }
 send() { echo ">" "$@" >&2;
@@ -296,86 +195,6 @@ fail_with() {
   return 1
 }
 
-serve_file() {
-  local file="$1"
-  local length="$(stat -c'%s' "$file")"
-  if [ $? -ne 0 ]; then
-    fail_with 404
-  else
-    CONTENT_TYPE=
-    case "$file" in
-      *\.css)
-        CONTENT_TYPE="text/css"
-        ;;
-      *\.js)
-        CONTENT_TYPE="text/javascript"
-        ;;
-      *)
-        CONTENT_TYPE="$(file -b --mime-type "$file")"
-        ;;
-    esac
-
-    set_response_header "Content-Type" "$CONTENT_TYPE"
-
-    read -r CONTENT_LENGTH < <(stat -c'%s' "$file") && \
-      set_response_header "Content-Length" "$CONTENT_LENGTH"
-
-    cat "$file"
-  fi
-}
-
-serve_dir_with_tree() {
-  local dir="$1" tree_vers tree_opts basehref x
-
-  set_response_header "Content-Type" "text/html"
-
-  # The --du option was added in 1.6.0.
-  read x tree_vers x < <(tree --version)
-  [[ $tree_vers == v1.6* ]] && tree_opts="--du"
-
-  tree -H "$2" -L 1 "$tree_opts" -D "$dir"
-}
-
-serve_dir_with_ls() {
-  local dir=$1
-  set_response_header "Content-Type" "text/plain"
-  ls -la "$dir"
-}
-
-serve_dir() {
-  # If `tree` is installed, use that for pretty output.
-  if which tree &>/dev/null; then
-    serve_dir_with_tree "$@"
-  else
-    serve_dir_with_ls "$@"
-  fi
-}
-
-serve_dir_or_file_from() {
-  local root="$1"
-  local file="${3-${2-}}"
-  local URL_PATH="${root}/${file}"
-
-  # sanitize URL_PATH
-  URL_PATH=${URL_PATH//[^a-zA-Z0-9_~\-\.\/]/}
-  [[ $URL_PATH == *..* ]] && fail_with 400
-
-  # Serve index file if exists in requested directory
-  [[ -d $URL_PATH && -f $URL_PATH/index.html && -r $URL_PATH/index.html ]] && \
-    URL_PATH="$URL_PATH/index.html"
-
-  if [[ -f $URL_PATH ]] && [[ -r $URL_PATH ]]; then
-    serve_file "$URL_PATH" "$@"
-  elif [[ -d $URL_PATH ]] && [[ -x $URL_PATH ]]; then
-    serve_dir  "$URL_PATH" "$@"
-  fi
-}
-
-serve_static_string() {
-  set_response_header "Content-Type" "text/plain"
-  echo "$1"
-}
-
 # https://stackoverflow.com/a/37840948/376773
 decode_url() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
 
@@ -394,7 +213,7 @@ get_request_header() {
   return 1
 }
 
-get_request_body() {
+parse_request_body() {
   local length="$(get_request_header "content-length")"
   if [ ! -z "${length}" ]; then
     recv "Reading ${length} byte request body"
@@ -426,18 +245,8 @@ get_request_body() {
   fi
 }
 
-on_uri_match() {
-  local regex=$1
-  shift
-  [[ $REQUEST_URI =~ $regex ]] && "$@" "${BASH_REMATCH[@]}"
-}
-
-unconditionally() {
-  "$@" "$REQUEST_URI"
-}
-
 run_user_code() {
   source "${conf}"
 }
 
-get_request_body | run_user_code | flush_response
+parse_request_body | run_user_code | flush_response
